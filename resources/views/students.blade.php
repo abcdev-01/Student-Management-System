300e737311020964f718ffcfdb183498bab544a4
abcdev-01 <martxon123@gmail.com>
2026-09-14 14:45:29 +0300

    Replace welcome view with students view and add students.blade.php template

---
+++ resources/views/students.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Student-Management-System</Student-Management-System></title>
+</head>
+
+<body>
+    <h1>Student Management System</h1>
+    <p>Welcome to the Student Management System!</p>
+</body>
+
+</html>
